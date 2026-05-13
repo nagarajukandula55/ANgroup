b448d6bae1b6faf7fd14b86bfd232776587f6780
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-13 16:44:35 +0530

    Update route.ts

--- src/app/api/payment/verify/route.ts
+++ src/app/api/payment/verify/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectDB from "@/lib/mongodb";
+import { connectDB } from "@/lib/mongodb";
 import crypto from "crypto";
 import Order from "@/models/Order";
 import { processOrderAfterPayment } from "@/services/orderPipeline.service";
