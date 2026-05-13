78b23f1426084309de1b644687d72b653fd6b0c9
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-13 23:35:43 +0530

    Create DocumentCounter.ts

---
+++ src/models/DocumentCounter.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const DocumentCounterSchema = new mongoose.Schema(
+  {
+    businessId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+
+    documentType: {
+      type: String,
+      required: true,
+      index: true,
+    },
+
+    financialYear: {
+      type: String,
+      required: true,
+      index: true,
+    },
+
+    prefix: {
+      type: String,
+      required: true,
+    },
+
+    current: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+DocumentCounterSchema.index(
+  {
+    businessId: 1,
+    documentType: 1,
+    financialYear: 1,
+  },
+  {
+    unique: true,
+  }
+);
+
+export default
+  mongoose.models.DocumentCounter ||
+  mongoose.model(
+    "DocumentCounter",
+    DocumentCounterSchema
+  );
