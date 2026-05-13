e4718e54c8dcbe919af75d88c878e50f33d01bce
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-13 23:42:36 +0530

    Create documentTypes.ts

---
+++ src/constants/documentTypes.ts
@@ -0,0 +1,21 @@
+export const DOCUMENT_TYPES = {
+  TAX: "TAX",
+
+  B2B: "B2B",
+
+  B2C: "B2C",
+
+  POS: "POS",
+
+  PROFORMA: "PROFORMA",
+
+  QUOTE: "QUOTE",
+
+  CN: "CN",
+
+  DN: "DN",
+
+  PO: "PO",
+
+  PI: "PI",
+} as const;
