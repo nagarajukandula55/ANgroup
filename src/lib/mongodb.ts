a4fc1bb85b6401b5e68c314bd886321f4677c40f
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-13 13:23:05 +0530

    Update mongodb.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,28 +3,13 @@ import mongoose from 'mongoose'
 const MONGODB_URI = process.env.MONGODB_URI!
 
 if (!MONGODB_URI) {
-  throw new Error('Missing MongoDB URI')
-}
-
-let cached = (global as any).mongoose
-
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  }
+  throw new Error('MONGODB_URI is missing')
 }
 
 export async function connectDB() {
-  if (cached.conn) {
-    return cached.conn
+  if (mongoose.connections[0].readyState) {
+    return
   }
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI)
-  }
-
-  cached.conn = await cached.promise
-
-  return cached.conn
+  await mongoose.connect(MONGODB_URI)
 }
