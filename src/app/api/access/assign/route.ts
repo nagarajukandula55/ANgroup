c0f5db75c80ee60ad00c6e384d8f12f95482fa45
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-13 15:08:36 +0530

    Create route.ts

---
+++ src/app/api/access/assign/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/mongodb";
+import { assignAccess } from "@/services/access.service";
+
+export async function POST(req: Request) {
+  try {
+    await connectDB();
+
+    const body = await req.json();
+
+    const result = await assignAccess(body);
+
+    return NextResponse.json({
+      success: true,
+      access: result,
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: err.message,
+      },
+      { status: 500 }
+    );
+  }
+}
