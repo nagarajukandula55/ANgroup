3b3743282057c66c86f2fc58bfc455998d5626cf
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-16 04:05:33 +0530

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -16,7 +16,7 @@ export default function Layout({
 
       <Sidebar />
 
-      <main className="relative z-10 flex-1 overflow-y-auto p-8 lg:p-10">
+      <main className="relative z-10 flex-1 overflow-y-auto p-4 md:p-6 lg:p-10">
         <div className="min-h-[calc(100vh-2rem)] rounded-[36px] border border-white/10 bg-white/[0.025] shadow-2xl backdrop-blur-2xl">
           <div className="p-6 lg:p-10">
             {children}
