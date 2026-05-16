d1880ebec01900a5139591af49d2031cf420e304
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-16 12:56:19 +0530

    Create order.types.ts

---
+++ src/types/order.types.ts
@@ -0,0 +1,22 @@
+export type CartBaseItem = {
+  productId: string;
+  productKey: string;
+  name: string;
+  qty: number;
+  sellingPrice: number;
+  gstRate: number;
+  baseTotal: number;
+};
+
+export type CartWithDiscount = CartBaseItem & {
+  discount: number;
+  taxableValue: number;
+};
+
+export type CartWithGST = CartWithDiscount & {
+  gstAmount: number;
+  cgst: number;
+  sgst: number;
+  igst: number;
+  lineTotal: number;
+};
