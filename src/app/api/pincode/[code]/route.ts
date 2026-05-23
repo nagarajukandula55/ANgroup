3e13204b64fa4f8731c527337e52f6b808386d20
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-23 22:57:32 +0530

    Update route.ts

--- src/app/api/pincode/[code]/route.ts
+++ src/app/api/pincode/[code]/route.ts
@@ -5,24 +5,49 @@ export async function GET(
   { params }: { params: { code: string } }
 ) {
   try {
-    const res = await fetch(
+    const response = await fetch(
       `https://api.postalpincode.in/pincode/${params.code}`,
       {
         cache: "no-store",
       }
     );
 
-    const data = await res.json();
+    const data = await response.json();
 
-    return NextResponse.json(data);
+    if (
+      !data?.[0] ||
+      data[0].Status !== "Success"
+    ) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "Invalid pincode",
+        },
+        { status: 404 }
+      );
+    }
+
+    const po =
+      data[0]?.PostOffice?.[0];
+
+    return NextResponse.json({
+      success: true,
+      city: po?.District || "",
+      state: po?.State || "",
+      country: po?.Country || "India",
+    });
 
   } catch (err: any) {
+    console.error("PINCODE API ERROR:", err);
+
     return NextResponse.json(
       {
         success: false,
         error: err.message,
       },
-      { status: 500 }
+      {
+        status: 500,
+      }
     );
   }
 }
