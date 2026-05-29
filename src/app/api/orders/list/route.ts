62cf66e81fe753ffed65855daf9915dd1f90bcda
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-29 20:30:12 +0530

    Create route.ts

---
+++ src/app/api/orders/list/route.ts
@@ -0,0 +1,36 @@
+export const runtime = "nodejs";
+
+import { NextResponse } from "next/server";
+
+import { connectDB } from "@/lib/mongodb";
+
+import Order from "@/models/Order";
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const orders = await Order.find({})
+      .sort({ createdAt: -1 })
+      .lean();
+
+    return NextResponse.json({
+      success: true,
+      orders,
+    });
+  } catch (error: any) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          error.message ||
+          "Failed to fetch orders",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
