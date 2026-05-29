2f650246b9a5cfce9befef4d1f33e6871024b058
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-30 00:49:24 +0530

    Create route.ts

---
+++ src/app/api/invoice/mark-generated/route.ts
@@ -0,0 +1,81 @@
+export const runtime = "nodejs";
+
+import { NextResponse } from "next/server";
+
+import { connectDB } from "@/lib/mongodb";
+
+import Order from "@/models/Order";
+
+export async function POST(req: Request) {
+  try {
+    await connectDB();
+
+    const body = await req.json();
+
+    const {
+      orderId,
+      invoiceNumber,
+      invoiceUrl,
+    } = body;
+
+    const order = await Order.findOne({
+      orderId,
+    });
+
+    if (!order) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Order not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    order.invoiceGenerated = true;
+
+    order.invoice = {
+      ...order.invoice,
+
+      invoiceNumber,
+
+      invoiceUrl,
+
+      generatedAt: new Date(),
+
+      pdfGenerated: true,
+    };
+
+    order.events.push({
+      type: "INVOICE_GENERATED",
+
+      message:
+        "Invoice generated successfully",
+
+      by: "SYSTEM",
+
+      createdAt: new Date(),
+    });
+
+    await order.save();
+
+    return NextResponse.json({
+      success: true,
+      order,
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          error.message ||
+          "Invoice update failed",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
