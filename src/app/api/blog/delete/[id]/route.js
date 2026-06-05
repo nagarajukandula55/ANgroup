1f96d302409af6a33eb3596b40d660859b5e6b6b
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-05 17:25:22 +0530

    Create route.js

---
+++ src/app/api/blog/delete/[id]/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/an-db";
+import Blog from "@/models/Blog";
+
+export async function DELETE(req, { params }) {
+  try {
+    await connectDB();
+
+    await Blog.findByIdAndDelete(params.id);
+
+    return NextResponse.json({ success: true });
+  } catch (err) {
+    return NextResponse.json(
+      { success: false, message: err.message },
+      { status: 500 }
+    );
+  }
+}
