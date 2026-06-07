ef60067f4076ad31f25d87bfe7755a2d36571682
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-07 09:00:01 +0530

    Update route.js

--- src/app/api/blog/list/route.js
+++ src/app/api/blog/list/route.js
@@ -1,18 +1,28 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/an-db";
+import { connectDB } from "@/lib/mongodb";
 import Blog from "@/models/Blog";
 
 export async function GET() {
   try {
     await connectDB();
 
-    const blogs = await Blog.find().sort({ createdAt: -1 });
+    const blogs = await Blog.find().sort({
+      createdAt: -1,
+    });
 
-    return NextResponse.json({ success: true, blogs });
+    return NextResponse.json({
+      success: true,
+      blogs,
+    });
   } catch (err) {
     return NextResponse.json(
-      { success: false, message: err.message },
-      { status: 500 }
+      {
+        success: false,
+        message: err.message,
+      },
+      {
+        status: 500,
+      }
     );
   }
 }
