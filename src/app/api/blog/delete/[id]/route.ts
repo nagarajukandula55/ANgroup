0ef99068ae74aba180302116331272e9b894152c
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-07 09:19:26 +0530

    Update route.ts

--- src/app/api/blog/delete/[id]/route.ts
+++ src/app/api/blog/delete/[id]/route.ts
@@ -1,20 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import Blog from "@/models/Blog";
 
 export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
+  request: Request,
+  context: { params: Promise<{ id: string }> }
 ) {
   try {
     await connectDB();
 
-    await Blog.findByIdAndDelete(params.id);
+    const { id } = await context.params;
+
+    await Blog.findByIdAndDelete(id);
 
     return NextResponse.json({
       success: true,
     });
   } catch (error: any) {
+    console.error("BLOG DELETE ERROR:", error);
+
     return NextResponse.json(
       {
         success: false,
