2f994ddf679e292248c97892b246438cc1d42792
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-16 23:16:43 +0530

    Create Asset.js

---
+++ src/models/Asset.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const AssetSchema = new mongoose.Schema(
+  {
+    name: String,
+
+    category: {
+      type: String,
+      enum: [
+        "logo",
+        "product-image",
+        "icon",
+        "certification",
+        "background",
+        "shape",
+      ],
+    },
+
+    fileUrl: String,
+
+    thumbnailUrl: String,
+
+    tags: [String],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Asset ||
+  mongoose.model("Asset", AssetSchema);
