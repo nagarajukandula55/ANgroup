dc5f5284b1a117710cf18dd8657e2ca3676a208f
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-24 23:25:05 +0530

    Create page.tsx

---
+++ src/app/admin/masters/material-categories/page.tsx
@@ -0,0 +1,16 @@
+import PageHeader from "@/components/admin/PageHeader";
+
+export default function MaterialCategoriesPage() {
+  return (
+    <div className="p-8">
+      <PageHeader
+        title="Material Categories"
+        subtitle="Manage material groups"
+      />
+
+      <div className="rounded-2xl border border-white/10 p-6">
+        Material Categories Module
+      </div>
+    </div>
+  );
+}
