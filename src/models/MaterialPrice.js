e3713caf3ce54782c8a203a838c664dbe2e1e431
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-24 22:50:06 +0530

    Update MaterialPrice.js

--- src/models/MaterialPrice.js
+++ src/models/MaterialPrice.js
@@ -1,57 +1,95 @@
 import mongoose from "mongoose";
 
 const MaterialPriceSchema = new mongoose.Schema(
-{
-  materialId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Material",
-    required: true,
-  },
+  {
+    materialId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Material",
+      required: true,
+      index: true,
+    },
 
-  currentPrice: {
-    type: Number,
-    required: true,
-  },
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+    },
 
-  expectedPrice: {
-    type: Number,
-    default: 0,
-  },
+    locationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Location",
+    },
 
-  worstCasePrice: {
-    type: Number,
-    default: 0,
-  },
+    currentPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
 
-  averagePrice: {
-    type: Number,
-    default: 0,
-  },
+    expectedPrice: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
 
-  lowestPrice: {
-    type: Number,
-    default: 0,
-  },
+    worstCasePrice: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
 
-  highestPrice: {
-    type: Number,
-    default: 0,
-  },
+    averagePrice: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
 
-  primaryVendorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Vendor",
-  },
+    lowestPrice: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
 
-  effectiveDate: {
-    type: Date,
-    default: Date.now,
+    highestPrice: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+
+    priceUnit: {
+      type: String,
+      default: "KG",
+    },
+
+    isPreferredVendor: {
+      type: Boolean,
+      default: false,
+    },
+
+    remarks: {
+      type: String,
+      default: "",
+    },
+
+    effectiveDate: {
+      type: Date,
+      default: Date.now,
+    },
+
+    active: {
+      type: Boolean,
+      default: true,
+    },
   },
-},
-{
-  timestamps: true,
-}
+  {
+    timestamps: true,
+  }
 );
 
+MaterialPriceSchema.index({
+  materialId: 1,
+  vendorId: 1,
+  effectiveDate: -1,
+});
+
 export default mongoose.models.MaterialPrice ||
-mongoose.model("MaterialPrice", MaterialPriceSchema);
+  mongoose.model("MaterialPrice", MaterialPriceSchema);
