dbb10f8154a081a08f9aa5394a125c88cce52597
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-24 23:23:31 +0530

    Create page.tsx

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+export default function AdminDashboard() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">
+        AN Dashboard
+      </h1>
+
+      <p className="mt-2 text-zinc-400">
+        Unified Enterprise Platform
+      </p>
+    </div>
+  );
+}
