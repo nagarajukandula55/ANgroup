93ef91a9fb1fd684a72fca3f8088821836e3f998
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-24 22:51:05 +0530

    Update MaterialPriceHistory.js

--- src/models/MaterialPriceHistory.js
+++ src/models/MaterialPriceHistory.js
@@ -1,38 +1,79 @@
 import mongoose from "mongoose";
 
-const MaterialPriceHistorySchema =
-new mongoose.Schema(
-{
-  materialId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Material",
-    required: true,
-  },
+const MaterialPriceHistorySchema = new mongoose.Schema(
+  {
+    materialId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Material",
+      required: true,
+      index: true,
+    },
 
-  vendorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Vendor",
-  },
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+    },
 
-  price: {
-    type: Number,
-    required: true,
-  },
+    locationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Location",
+    },
 
-  effectiveDate: {
-    type: Date,
-    required: true,
-  },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
 
-  remarks: String,
-},
-{
-  timestamps: true,
-}
+    priceUnit: {
+      type: String,
+      default: "KG",
+    },
+
+    effectiveDate: {
+      type: Date,
+      required: true,
+    },
+
+    source: {
+      type: String,
+      enum: [
+        "MANUAL",
+        "PURCHASE_ORDER",
+        "IMPORT",
+        "SYSTEM",
+      ],
+      default: "MANUAL",
+    },
+
+    remarks: {
+      type: String,
+      default: "",
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
+
+MaterialPriceHistorySchema.index({
+  materialId: 1,
+  effectiveDate: -1,
+});
+
+MaterialPriceHistorySchema.index({
+  materialId: 1,
+  vendorId: 1,
+  effectiveDate: -1,
+});
 
 export default mongoose.models.MaterialPriceHistory ||
-mongoose.model(
-  "MaterialPriceHistory",
-  MaterialPriceHistorySchema
-);
+  mongoose.model(
+    "MaterialPriceHistory",
+    MaterialPriceHistorySchema
+  );
