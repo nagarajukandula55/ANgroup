afb86f197d59db9c2ae68d3f0bdc5aff1da41446
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 14:04:12 +0530

    Update route.ts

--- src/app/api/grn/[id]/route.ts
+++ src/app/api/grn/[id]/route.ts
@@ -1,14 +1,11 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/mongodb";
-import GRN from "@/models/GRN";
+import { getGRNById } from "@/services/grn.service";
 
 export async function GET(_: Request, { params }: any) {
   await dbConnect();
 
-  const data = await GRN.findById(params.id)
-    .populate("poId")
-    .populate("vendorId")
-    .populate("items.materialId");
+  const data = await getGRNById(params.id);
 
   return NextResponse.json({ success: true, data });
 }
