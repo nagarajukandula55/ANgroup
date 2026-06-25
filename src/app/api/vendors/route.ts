7de1a05312aebfc2daec828617bc62526d59b0b0
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 15:23:52 +0530

    Update route.ts

--- src/app/api/vendors/route.ts
+++ src/app/api/vendors/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/mongodb";
+import { dbConnect } from "@/lib/mongodb";
 import Vendor from "@/models/Vendor";
 
 export async function GET() {
