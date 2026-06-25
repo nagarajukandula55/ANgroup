231b5be4818afc9e09584770da2af33cffa2b17b
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-26 01:30:23 +0530

    Create VendorProductBOM.js

---
+++ src/models/VendorProductBOM.js
@@ -0,0 +1,96 @@
+import mongoose from "mongoose";
+
+const VendorProductBOMSchema =
+  new mongoose.Schema(
+    {
+      businessId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Business",
+      },
+
+      vendorProductId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "VendorProduct",
+        required: true,
+        index: true,
+      },
+
+      materialId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Material",
+        required: true,
+      },
+
+      materialCode: {
+        type: String,
+      },
+
+      materialName: {
+        type: String,
+      },
+
+      quantity: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+
+      unit: {
+        type: String,
+        required: true,
+      },
+
+      wastagePercent: {
+        type: Number,
+        default: 0,
+      },
+
+      currentRate: {
+        type: Number,
+        default: 0,
+      },
+
+      currentCost: {
+        type: Number,
+        default: 0,
+      },
+
+      remarks: {
+        type: String,
+        default: "",
+      },
+
+      active: {
+        type: Boolean,
+        default: true,
+      },
+
+      createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+VendorProductBOMSchema.index({
+  vendorProductId: 1,
+});
+
+VendorProductBOMSchema.index({
+  materialId: 1,
+});
+
+VendorProductBOMSchema.index({
+  vendorProductId: 1,
+  materialId: 1,
+});
+
+export default
+  mongoose.models.VendorProductBOM ||
+  mongoose.model(
+    "VendorProductBOM",
+    VendorProductBOMSchema
+  );
