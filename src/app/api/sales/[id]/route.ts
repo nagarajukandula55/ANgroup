eb7e8ac194a7301a65c6b908f4be3f04af7f349e
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-26 00:44:13 +0530

    Update route.ts

--- src/app/api/sales/[id]/route.ts
+++ src/app/api/sales/[id]/route.ts
@@ -3,7 +3,7 @@ import { connectDB } from "@/lib/mongodb";
 import { getInvoiceById } from "@/services/sales.service";
 
 export async function GET(_: Request, { params }: any) {
-  await dbConnect();
+  await connectDB();
 
   const data = await getInvoiceById(params.id);
 
