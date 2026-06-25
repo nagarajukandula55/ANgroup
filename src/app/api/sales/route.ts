9ed7b8aed8790f0faa5cc9800d1b8647f20549a2
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 15:17:09 +0530

    Create route.ts

---
+++ src/app/api/sales/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongodb";
+import {
+  createSalesInvoice,
+  getAllInvoices,
+} from "@/services/sales.service";
+
+/* ================= CREATE INVOICE ================= */
+export async function POST(req: Request) {
+  try {
+    await dbConnect();
+
+    const body = await req.json();
+
+    const data = await createSalesInvoice(body);
+
+    return NextResponse.json({
+      success: true,
+      data,
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      { success: false, message: err.message },
+      { status: 400 }
+    );
+  }
+}
+
+/* ================= LIST INVOICES ================= */
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const data = await getAllInvoices();
+
+    return NextResponse.json({
+      success: true,
+      data,
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      { success: false, message: err.message },
+      { status: 500 }
+    );
+  }
+}
