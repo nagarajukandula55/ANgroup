ac0b25e2727309088ffac7e0a39cc00c2a080ab2
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-26 02:05:56 +0530

    Create route.ts

---
+++ src/app/api/vendor-catalog/product/[variantId]/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/mongodb";
+
+import VendorCatalog from "@/models/VendorCatalog";
+
+export async function GET(
+  req: Request,
+  context: any
+) {
+  try {
+    await connectDB();
+
+    const vendors =
+      await VendorCatalog.find({
+        variantId:
+          context.params.variantId,
+
+        active: true,
+
+        status: "ACTIVE",
+      })
+        .populate("vendorId")
+        .sort({
+          preferredVendor: -1,
+          priority: 1,
+          vendorCost: 1,
+        });
+
+    return NextResponse.json({
+      success: true,
+      data: vendors,
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: err.message,
+      },
+      { status: 500 }
+    );
+  }
+}
