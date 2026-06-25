53846c366b25b38dcf295937099755e550ea7873
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 14:03:25 +0530

    Update route.ts

--- src/app/api/purchase-orders/route.ts
+++ src/app/api/purchase-orders/route.ts
@@ -1,24 +1,18 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/mongodb";
-import PurchaseOrder from "@/models/PurchaseOrder";
+import {
+  createPurchaseOrder,
+  getAllPurchaseOrders,
+} from "@/services/purchaseOrder.service";
 
-function generatePONumber() {
-  return "PO-" + Date.now();
-}
-
-/* ================= CREATE PO ================= */
 export async function POST(req: Request) {
   try {
     await dbConnect();
 
     const body = await req.json();
+    const data = await createPurchaseOrder(body);
 
-    const po = await PurchaseOrder.create({
-      ...body,
-      poNumber: generatePONumber(),
-    });
-
-    return NextResponse.json({ success: true, data: po });
+    return NextResponse.json({ success: true, data });
   } catch (err: any) {
     return NextResponse.json(
       { success: false, message: err.message },
@@ -27,15 +21,11 @@ export async function POST(req: Request) {
   }
 }
 
-/* ================= LIST PO ================= */
 export async function GET() {
   try {
     await dbConnect();
 
-    const data = await PurchaseOrder.find()
-      .populate("vendorId")
-      .populate("items.materialId")
-      .sort({ createdAt: -1 });
+    const data = await getAllPurchaseOrders();
 
     return NextResponse.json({ success: true, data });
   } catch (err: any) {
