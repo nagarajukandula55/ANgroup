95a322fad617cf9e0ab87631e45147245e05480a
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 14:05:47 +0530

    Create route.ts

---
+++ src/app/api/stock/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongodb";
+import StockLedger from "@/models/StockLedger";
+
+export async function GET() {
+  await dbConnect();
+
+  const data = await StockLedger.find()
+    .populate("materialId")
+    .populate("warehouseId")
+    .sort({ createdAt: -1 });
+
+  return NextResponse.json({ success: true, data });
+}
