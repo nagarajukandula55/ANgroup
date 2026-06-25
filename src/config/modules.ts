ce4b41f5fe84e54f7b1a694a4f16512985c079dc
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 11:32:29 +0530

    Update modules.ts

--- src/config/modules.ts
+++ src/config/modules.ts
@@ -2,18 +2,7 @@ export const MODULES = {
   DASHBOARD: "dashboard",
 
   MASTERS: "masters",
-  UNITS: "units",
-  VENDORS: "vendors",
-  BRANDS: "brands",
-  MATERIAL_CATEGORIES: "material-categories",
-  PRODUCT_CATEGORIES: "product-categories",
-
   MATERIALS: "materials",
-  MATERIAL_PRICING: "material-pricing",
-
   PRODUCTS: "products",
-  PRODUCT_VARIANTS: "product-variants",
-
   MANUFACTURING: "manufacturing",
-  BOM: "bom",
-} as const;
+};
