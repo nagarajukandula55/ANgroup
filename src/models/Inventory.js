77479de6115433efacfe11b97c4a5ea284f33ff4
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 13:48:06 +0530

    Update Inventory.js

--- src/models/Inventory.js
+++ src/models/Inventory.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const InventorySchema = new mongoose.Schema(
   {
-    companyId: {
+    businessId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Company",
+      ref: "Business",
     },
 
     warehouseId: {
