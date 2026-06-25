e6e3a02cd92e44d6d739e0ba5c5cfcd400c18024
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 15:08:46 +0530

    Create route.ts

---
+++ src/app/api/stock/out/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongodb";
+import { stockOut } from "@/services/stock.service";
+
+export async function POST(req: Request) {
+  try {
+    await dbConnect();
+
+    const body = await req.json();
+
+    const data = await stockOut(body);
+
+    return NextResponse.json({
+      success: true,
+      data,
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      { success: false, message: err.message },
+      { status: 400 }
+    );
+  }
+}
