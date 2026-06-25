416fe352fb059b94b6b53f8c4e3bcded15676134
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-26 00:36:55 +0530

    Update route.ts

--- src/app/api/grn/[id]/route.ts
+++ src/app/api/grn/[id]/route.ts
@@ -3,7 +3,7 @@ import { connectDB } from "@/lib/mongodb";
 import { getGRNById } from "@/services/grn.service";
 
 export async function GET(_: Request, { params }: any) {
-  await dbConnect();
+  await connectDB();
 
   const data = await getGRNById(params.id);
 
