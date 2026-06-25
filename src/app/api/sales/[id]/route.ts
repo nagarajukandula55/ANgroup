bedf24e6269f072218859d0333e71acee17837b3
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 15:17:31 +0530

    Create route.ts

---
+++ src/app/api/sales/[id]/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongodb";
+import { getInvoiceById } from "@/services/sales.service";
+
+export async function GET(_: Request, { params }: any) {
+  await dbConnect();
+
+  const data = await getInvoiceById(params.id);
+
+  return NextResponse.json({
+    success: true,
+    data,
+  });
+}
