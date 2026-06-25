684a83416aaa2c24ec7370c6a91b92bf2e385016
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-25 15:27:25 +0530

    Update route.ts

--- src/app/api/grn/[id]/route.ts
+++ src/app/api/grn/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { dbConnect } from "@/lib/mongodb";
+import { connectDB } from "@/lib/mongodb";
 import { getGRNById } from "@/services/grn.service";
 
 export async function GET(_: Request, { params }: any) {
