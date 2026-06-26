404e83de9e50b934a17ea26798ab8c6e01d7969f
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-26 19:26:11 +0530

    Update page.tsx

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,9 +1,31 @@
-export default async function Page({ params }) {
-  await connectDB(); // ✅ inside function
+import { connectDB } from "@/lib/mongodb";
+import Product from "@/models/Product";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function Page({
+  params,
+}: PageProps) {
+  await connectDB();
+
+  const { slug } = await params;
 
   const product = await Product.findOne({
-    "seo.slug": params.slug,
+    "seo.slug": slug,
   }).populate("variants");
 
-  return <div>{product?.productName}</div>;
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <h1>{product.productName}</h1>
+    </div>
+  );
 }
