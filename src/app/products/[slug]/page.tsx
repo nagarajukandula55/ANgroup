b475343cc29ac4d7dff42f60952aa97961561434
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-26 17:10:09 +0530

    Update page.tsx

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,9 @@
-const product = await Product.findOne({
-  "seo.slug": params.slug,
-}).populate("variants");
+export default async function Page({ params }) {
+  await connectDB(); // ✅ inside function
+
+  const product = await Product.findOne({
+    "seo.slug": params.slug,
+  }).populate("variants");
+
+  return <div>{product?.productName}</div>;
+}
