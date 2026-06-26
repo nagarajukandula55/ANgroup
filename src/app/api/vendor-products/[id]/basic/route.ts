73ff5888e569111c5714e637e104c4f4a197a843
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-26 17:36:01 +0530

    Update route.ts

--- src/app/api/vendor-products/[id]/basic/route.ts
+++ src/app/api/vendor-products/[id]/basic/route.ts
@@ -2,17 +2,22 @@ import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import VendorProduct from "@/models/VendorProduct";
 
-export async function PATCH(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function PATCH(req: Request, context: Context) {
   try {
     await connectDB();
+
+    const { id } = context.params;
 
     const body = await req.json();
 
     const updated = await VendorProduct.findByIdAndUpdate(
-      params.id,
+      id,
       body,
       { new: true }
     );
