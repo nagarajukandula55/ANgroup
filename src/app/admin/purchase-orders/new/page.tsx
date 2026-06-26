f1e1933546761fe114964247eaea726cebc05ba9
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-26 17:02:02 +0530

    Create page.tsx

---
+++ src/app/admin/purchase-orders/new/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import PurchaseOrderForm from "@/components/purchase-order/PurchaseOrderForm";
+
+export default function NewPurchaseOrderPage() {
+  return (
+    <div className="p-6">
+      <PurchaseOrderForm />
+    </div>
+  );
+}
